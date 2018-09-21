1b1e6f1d171c1ee0c8646b4f723702160affe900
JANGHUIJO <jhjo.tech@gmail.com>
2018-09-21 16:54:04 +0900

    네비게이션 커스텀 [1] 네이게이션 바 타이틀 초기화 메서드 준비

---
+++ Practice Example/NavigationBarCustom/NavigationBarCustom/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NavigationBarCustom
+//
+//  Created by Jo JANGHUI on 2018. 9. 21..
+//  Copyright © 2018년 JhDAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initTitle()
+    }
+
+    func initTitle() {
+        
+    }
+
+}
+
