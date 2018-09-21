942ff7fe14f222924d7b7092b395a9340537c5bb
JANGHUIJO <jhjo.tech@gmail.com>
2018-09-21 19:16:18 +0900

    네비게이션 커스텀 [2] 텍스트 필드 넣어보기, 우측 좌측 네이게이션 아이템 커스텀 하기

--- Practice Example/NavigationBarCustom/NavigationBarCustom/ViewController.swift
+++ Practice Example/NavigationBarCustom/NavigationBarCustom/ViewController.swift
@@ -12,10 +12,102 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        initTitle()
+//        initTitle()
+//        initTitleNew()
+//        initTitleImage()
+        initTitleImput()
     }
 
     func initTitle() {
+        let nTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        
+        nTitle.numberOfLines = 2
+        nTitle.textAlignment = .center
+        nTitle.textColor = .white
+        nTitle.font = UIFont.systemFont(ofSize: 15)
+        nTitle.text = "58개 숙소 \n 1박(1월 10일 ~ 1월 11일"
+        
+        self.navigationItem.titleView = nTitle
+        
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+    }
+    
+    func initTitleNew () {
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 36))
+        
+        let topTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 18))
+        topTitle.numberOfLines = 1
+        topTitle.textAlignment = .center
+        topTitle.font = UIFont.systemFont(ofSize: 15)
+        topTitle.textColor = .white
+        topTitle.text = "58개 숙소"
+        
+        let subTitle = UILabel(frame: CGRect(x: 0, y: 18, width: 200, height: 18))
+        subTitle.numberOfLines = 1
+        subTitle.textAlignment = .center
+        subTitle.font = UIFont.systemFont(ofSize: 12)
+        subTitle.textColor = .white
+        subTitle.text = "1박(1월 10일 ~ 1월 11일"
+        
+        containerView.addSubview(topTitle)
+        containerView.addSubview(subTitle)
+        
+        self.navigationItem.titleView = containerView
+        
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+        
+    }
+    
+    func initTitleImage() {
+        let image = UIImage(named: "swift")
+        let imageV = UIImageView(image: image)
+        
+        self.navigationItem.titleView = imageV
+    }
+    
+    func initTitleImput() {
+        let back = UIImage(named: "arrow-back")
+        let leftItem = UIBarButtonItem(image: back, style: .plain, target: self, action: nil)
+        self.navigationItem.leftBarButtonItem = leftItem
+        
+        let rv = UIView()
+        rv.frame = CGRect(x: 0, y: 0, width: 70, height: 37)
+        let rItem = UIBarButtonItem(customView: rv)
+        self.navigationItem.rightBarButtonItem = rItem
+        
+        let cnt = UILabel()
+        cnt.frame = CGRect(x: 10, y: 8, width: 20, height: 20)
+        cnt.font = UIFont.boldSystemFont(ofSize: 10)
+        cnt.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
+        cnt.text = "12"
+        cnt.textAlignment = .center
+        
+        cnt.layer.cornerRadius = 3
+        cnt.layer.borderWidth = 2
+        cnt.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1).cgColor
+        rv.addSubview(cnt)
+        
+        let more = UIButton(type: .system)
+        more.frame = CGRect(x: 50, y: 10, width: 16, height: 16)
+        more.setImage(UIImage(named: "more"), for: .normal)
+        rv.addSubview(more)
+        
+        
+        let tf = UITextField()
+        tf.frame = CGRect(x: 0, y: 0, width: 300, height: 35)
+        tf.backgroundColor = .white
+        tf.font = UIFont.systemFont(ofSize: 13)
+        tf.autocapitalizationType = .none   // 자동 대문자
+        tf.autocorrectionType = .no         // 자동 입력 기능
+        tf.spellCheckingType = .no          // 스펠링 체크 기능
+        tf.keyboardType = .URL              // 키보드 타입
+        tf.keyboardAppearance = .dark
+        tf.layer.borderWidth = 0.3
+        tf.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1).cgColor
+        
+        self.navigationItem.titleView = tf
         
     }
 
