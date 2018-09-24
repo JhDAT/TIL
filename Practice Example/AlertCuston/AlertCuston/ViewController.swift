d343197ae041ce650f56c44397bb11b57491a06f
JANGHUIJO <jhjo.tech@gmail.com>
2018-09-24 17:49:21 +0900

    Alert 커스터마이징 [1]  - first init commit

---
+++ Practice Example/AlertCuston/AlertCuston/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertCuston
+//
+//  Created by Jo JANGHUI on 2018. 9. 24..
+//  Copyright © 2018년 JhDAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
